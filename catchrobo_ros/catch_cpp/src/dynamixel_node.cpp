2093c62ce05670bf66668e2954586f8f909a7060
Hiratsuka <hirekatsu0523@gmail.com>
2024-10-01 12:45:52 +0900

    update dynamixel_node

--- catchrobo_ros/catch_cpp/src/dynamixel_node.cpp
+++ catchrobo_ros/catch_cpp/src/dynamixel_node.cpp
@@ -9,14 +9,14 @@
 #include "dynamixel_sdk/dynamixel_sdk.h"
 #include "rclcpp/rclcpp.hpp"
 #include "../include/dynamixel_workbench_toolbox/dynamixel_workbench.h"
+#include "geometry_msgs/msg/quaternion.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
-#include "std_msgs/msg/float32.hpp"
 
 #define PROTOCOL_VERSION 2.0  // Default Protocol version of DYNAMIXEL X series.
 
 using namespace std::chrono_literals;
+using Quaternion = geometry_msgs::msg::Quaternion;
 using Float32MultiArray = std_msgs::msg::Float32MultiArray;
-using Float32 = std_msgs::msg::Float32;
 struct FingerConfig{
     uint8_t id;
     float min;
@@ -36,23 +36,32 @@ const std::vector<FingerConfig> finger_config = {
     {6, -179*M_PI/180, 179*M_PI/180, 0.0, false},
     {7, -179*M_PI/180, 179*M_PI/180, 0.0, false},
     {8, -179*M_PI/180, 179*M_PI/180, 0.0, false},
-    // {9, -179*M_PI/180, 179*M_PI/180, 0.0, false}
+    {9, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {10, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {11, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {12, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {13, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {14, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {15, -179*M_PI/180, 179*M_PI/180, 0.0, false},
+    {16, -179*M_PI/180, 179*M_PI/180, 0.0, false},
 };
-const FingerConfig wrist_config = {10, 0.0, 2*M_PI, 0.0, false};
-const char* DEVICE = "/dev/U2D2-0";
-const uint32_t BAUDRATE = 115200;
-const float load_limit = 70.0; // %
+const char* DEVICE = "/dev/U2D2-1";
+const uint32_t BAUDRATE = 1000000; // 1Mbps
+const float CURRENT = 30.0; // 100mA
 // ********************************************************************************************************************
 // クラスの定義
 // ********************************************************************************************************************
 class DynamixelNode : public rclcpp::Node{
     private:
-    rclcpp::Subscription<Float32MultiArray>::SharedPtr subscriber;
-    rclcpp::Subscription<Float32>::SharedPtr wrist_deg_sub;
+    rclcpp::Subscription<Quaternion>::SharedPtr sub1;
+    rclcpp::Subscription<Quaternion>::SharedPtr sub2;
+    rclcpp::Subscription<Quaternion>::SharedPtr sub3;
+    rclcpp::Subscription<Quaternion>::SharedPtr sub4;
+    rclcpp::Publisher<Float32MultiArray>::SharedPtr pub;
     rclcpp::TimerBase::SharedPtr timer;
     DynamixelWorkbench dxl_wb;
-    std::vector<float> target_position;
-    float wrist_target_pos = wrist_config.init;
+    std::vector<float> target = std::vector<float>(16, 0.0);
+    std::vector<float> now_angle = std::vector<float>(16, 0.0);
     const char* log;
 
     public:
@@ -77,83 +86,69 @@ class DynamixelNode : public rclcpp::Node{
                 std::cout << "検出されたモデル: XL_320" << std::endl;
             }else if(model_number == XL430_W250){
                 std::cout << "検出されたモデル: XL_430" << std::endl;
+            }else if(model_number == XL330_M288){
+                std::cout << "検出されたモデル: XL_330" << std::endl;
             }else{
                 std::cout << "検出されたモデル番号: " << model_number << std::endl;
             }
-            // dxl_wb.currentBasedPositionMode(config.id, current, &log);
-            dxl_wb.jointMode(config.id, 0, 0, &log);
-            // std::cout << "ID " << (int)config.id << " を電流ベース位置制御モードに設定\n" << log << std::endl;
-            std::cout << "ID " << (int)config.id << " をJointモードに設定\n" << log << std::endl;
+            dxl_wb.currentBasedPositionMode(config.id, dxl_wb.convertCurrent2Value(CURRENT), &log);
         }
-
-        dxl_wb.ping(wrist_config.id, &model_number, &log);
-        std::cout << "ID " << (int)wrist_config.id << " へPingを送信\n" << log << std::endl;
-        if(model_number == PRO_H42_20_S300_R_A){
-            std::cout << "検出されたモデル: PRO_H42_20_S300_R_A" << std::endl;
-        }else if(model_number == MX_106_2){
-            std::cout << "検出されたモデル: MX_106_2" << std::endl;
-        }else if(model_number == XM430_W350){
-            std::cout << "検出されたモデル: XM430_W350" << std::endl;
-        }else if(model_number == XL_320){
-            std::cout << "検出されたモデル: XL_320" << std::endl;
-        }else if(model_number == XL430_W250){
-            std::cout << "検出されたモデル: XL_430" << std::endl;
-        }else{
-            std::cout << "検出されたモデル番号: " << model_number << std::endl;
-        }
-        dxl_wb.jointMode(wrist_config.id, 0, 0, &log);
-        std::cout << "ID " << (int)wrist_config.id << " をJointモードに設定\n" << log << std::endl;
-
         std::cout << "セットアップが完了しました\n" << std::endl;
 
         auto timer_callback = [this]() -> void{
             for (int i = 0; i < finger_config.size(); i++){
-                // モータにかかっている負荷を取得
-                int32_t load;
-                dxl_wb.itemRead(finger_config[i].id, "Present_Load", &load, &log);
-                float load_percent = dxl_wb.convertValue2Load(load);
-                if (load_percent > load_limit){
-                    float present_position;
-                    dxl_wb.getRadian(finger_config[i].id, &present_position, &log);
-                    target_position[i] = present_position;
-                    std::cout << "over load " << load_percent << " > " << load_limit << std::endl;
-                    continue; // 負荷が設定値を超えている場合は動作しない
-                }
-                float target = target_position[i];
-                if (finger_config[i].reverse){
-                    target = -target;
-                }
-                target += finger_config[i].init;
-                target = std::max(finger_config[i].min, std::min(finger_config[i].max, target));
-                dxl_wb.goalPosition(finger_config[i].id, target, &log);
+                float target_ = target[i];
+                if (finger_config[i].reverse) target_ = -target_;
+                target_ += finger_config[i].init;
+                target_ = std::max(finger_config[i].min, std::min(finger_config[i].max, target_));
+                dxl_wb.goalPosition(finger_config[i].id, target_, &log);
+                dxl_wb.getRadian(finger_config[i].id, &now_angle[i], &log);
+                now_angle[i] = (now_angle[i] - finger_config[i].init) * (finger_config[i].reverse ? -1 : 1) * 180.0/M_PI;
             }
-            float wrist_target = wrist_target_pos;
-            if (wrist_config.reverse){
-                wrist_target = -wrist_target;
-            }
-            wrist_target += wrist_config.init;
-            wrist_target = std::max(wrist_config.min, std::min(wrist_config.max, wrist_target));
-            dxl_wb.goalPosition(wrist_config.id, wrist_target, &log); 
+            auto msg = Float32MultiArray();
+            msg.data = now_angle;
+            pub->publish(msg);
         };
         
-        auto topic_callback = [this](const Float32MultiArray::SharedPtr msg) -> void{
-            for (int i = 0; i < msg->data.size(); i++){
-                target_position[i] = msg->data[i]/180.0*M_PI;
-            }
+        auto topic_callback1 = [this](const Quaternion::SharedPtr msg) -> void{
+            target[0] = msg->x/180.0*M_PI;
+            target[1] = msg->y/180.0*M_PI;
+            target[2] = msg->z/180.0*M_PI;
+            target[3] = msg->w/180.0*M_PI;
         };
 
-        auto wrist_deg_callback = [this](const Float32::SharedPtr msg) -> void{
-            wrist_target_pos = msg->data/180.0*M_PI;
+        auto topic_callback2 = [this](const Quaternion::SharedPtr msg) -> void{
+            target[4] = msg->x/180.0*M_PI;
+            target[5] = msg->y/180.0*M_PI;
+            target[6] = msg->z/180.0*M_PI;
+            target[7] = msg->w/180.0*M_PI;
+        };
+
+        auto topic_callback3 = [this](const Quaternion::SharedPtr msg) -> void{
+            target[8] = msg->x/180.0*M_PI;
+            target[9] = msg->y/180.0*M_PI;
+            target[10] = msg->z/180.0*M_PI;
+            target[11] = msg->w/180.0*M_PI;
+        };
+
+        auto topic_callback4 = [this](const Quaternion::SharedPtr msg) -> void{
+            target[12] = msg->x/180.0*M_PI;
+            target[13] = msg->y/180.0*M_PI;
+            target[14] = msg->z/180.0*M_PI;
+            target[15] = msg->w/180.0*M_PI;
         };
 
         int8_t qos_depth = 10;
         const auto QOS_RKL10V = rclcpp::QoS(rclcpp::KeepLast(qos_depth)).reliable().durability_volatile();
-        subscriber =this->create_subscription<Float32MultiArray>("xl430_deg", QOS_RKL10V, topic_callback);
-        wrist_deg_sub = this->create_subscription<Float32>("wrist_deg", QOS_RKL10V, wrist_deg_callback);
-        for (auto config : finger_config){
-            target_position.push_back(config.init);
+        sub1 = this->create_subscription<Quaternion>("finger1_deg", QOS_RKL10V, topic_callback1);
+        sub2 = this->create_subscription<Quaternion>("finger2_deg", QOS_RKL10V, topic_callback2);
+        sub3 = this->create_subscription<Quaternion>("finger3_deg", QOS_RKL10V, topic_callback3);
+        sub4 = this->create_subscription<Quaternion>("finger4_deg", QOS_RKL10V, topic_callback4);
+        pub = this->create_publisher<Float32MultiArray>("finger_deg", QOS_RKL10V);
+        for (int i = 0; i < finger_config.size(); i++){
+            target[i] = finger_config[i].init;
         }
-        timer = this->create_wall_timer(2ms, timer_callback);
+        timer = this->create_wall_timer(10ms, timer_callback);
     }
 
     ~DynamixelNode(){
@@ -161,8 +156,6 @@ class DynamixelNode : public rclcpp::Node{
             dxl_wb.torqueOff(config.id, &log);
             std::cout << "ID " << (int)config.id << " のトルクをオフ\n" << log << std::endl;
         }
-        dxl_wb.torqueOff(wrist_config.id, &log);
-        std::cout << "ID " << (int)wrist_config.id << " のトルクをオフ\n" << log << std::endl;
     }
 };
 // ********************************************************************************************************************
